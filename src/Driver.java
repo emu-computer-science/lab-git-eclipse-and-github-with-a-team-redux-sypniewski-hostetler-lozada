9b5185d00357fa56ea77a464d4e4b5b14826a8b2
jjsyp <jsypniew@emich.edu>
2023-10-09 18:44:57 -0400

    added a sad function

--- src/Driver.java
+++ src/Driver.java
@@ -3,6 +3,8 @@ public class Driver {
 
 	public static void main(String[] args) {
 		System.out.println("Our team is getting started");
+		
+		Sad.print();
 
 	}
 
---
+++ src/Sad.java
@@ -0,0 +1,7 @@
+
+public class Sad {
+	
+	public static void print() {
+	System.out.println("Our team is sad");
+	}
+}
